9c1550f72f9ea35ae1ad0124690b2dc5735fcae3
Luis Rivera-Wong <lriverawong@outlook.com>
2019-02-27 00:27:10 -0500

    create deploy script and added Xresources and xinit dotfiles

---
+++ stow.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# needs `stow` package
+
+echo "=== Starting to link all dotfiles ==="
+
+stow -v compton
+stow -v github-config
+stow -v i3
+stow -v konsole
+stow -v neovim
+stow -v plasma-i3-replace
+stow -v profile
+stow -v ranger
+stow -v vim
+stow -v X
+stow -v zsh
+
+echo "=== Done linking all dotfiles ==="
