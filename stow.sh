bc7035bdf8f05178ceb9f9c5c9589b549e278652
Luis Rivera-Wong <lriverawong@outlook.com>
2019-06-15 13:09:12 -0400

    took off i3 and i3 replace from stow.sh

--- stow.sh
+++ stow.sh
@@ -6,10 +6,10 @@ echo "=== Starting to link all dotfiles ==="
 
 stow -v compton
 stow -v github-config
-stow -v i3
+# stow -v i3
 stow -v konsole
 stow -v neovim
-stow -v plasma-i3-replace
+# stow -v plasma-i3-replace
 stow -v profile
 stow -v ranger
 stow -v vim
