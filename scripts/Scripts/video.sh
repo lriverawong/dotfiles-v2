ee4e2fdab1b0cf4109af18331eb4d48ed8d7464d
Luis Rivera-Wong <luis.r@ecobee.com>
2019-08-06 21:30:25 -0400

    added script for workspace changes based on monitor setup

--- scripts/Scripts/video.sh
+++ scripts/Scripts/video.sh
@@ -3,9 +3,38 @@ internal=eDP1
 ecobee=DP1
 
 # triple-monitor-setup
-if xrandr | grep "$ecobee connected"; then
+if xrandr | grep -w "$ecobee connected"; then
     xrandr --output eDP1 --mode 1600x900 --pos 0x559 --rotate normal --output DP1 --primary --mode 2560x1440 --pos 1600x289 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 4160x0 --rotate right --output VIRTUAL1 --off
+    i3-msg "workspace 1, move workspace to output DP1"
+    i3-msg "workspace 2, move workspace to output DP1"
+    i3-msg "workspace 3, move workspace to output DP1"
+    i3-msg "workspace 4, move workspace to output DP1"
+    i3-msg "workspace 5, move workspace to output HDMI2"
+    i3-msg "workspace 6, move workspace to output HDMI2"
+    i3-msg "workspace 7, move workspace to output HDMI2"
+    i3-msg "workspace 8, move workspace to output eDP1"
+    nitrogen --restore; sleep 1; pkill compton; compton -b
+elif xrandr | grep -w "$ecobee disconnected"  && xrandr | grep -w "HDMI2 connected"; then
+    xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off
+    i3-msg "workspace 1, move workspace to output HDMI2"
+    i3-msg "workspace 2, move workspace to output HDMI2"
+    i3-msg "workspace 3, move workspace to output HDMI2"
+    i3-msg "workspace 4, move workspace to output HDMI2"
+    i3-msg "workspace 5, move workspace to output HDMI2"
+    i3-msg "workspace 6, move workspace to output HDMI2"
+    i3-msg "workspace 7, move workspace to output HDMI2"
+    i3-msg "workspace 8, move workspace to output eDP1"
+    nitrogen --restore; sleep 1; pkill compton; compton -b
 else
     # Solo laptop monitor
     xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x559 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+    i3-msg "workspace 1, move workspace to output eDP1"
+    i3-msg "workspace 2, move workspace to output eDP1"
+    i3-msg "workspace 3, move workspace to output eDP1"
+    i3-msg "workspace 4, move workspace to output eDP1"
+    i3-msg "workspace 5, move workspace to output eDP1"
+    i3-msg "workspace 6, move workspace to output eDP1"
+    i3-msg "workspace 7, move workspace to output eDP1"
+    i3-msg "workspace 8, move workspace to output eDP1"
+    nitrogen --restore; sleep 1; pkill compton; compton -b
 fi
