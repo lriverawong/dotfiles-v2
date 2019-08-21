1a217d41ea6da359b562bf60fa5a491461f2308f
Luis Rivera-Wong <lriverawong@outlook.com>
2019-08-21 19:03:35 -0400

    working on dunst font config as well as added standup mode to monitor script

--- scripts/Scripts/monitor.sh
+++ scripts/Scripts/monitor.sh
@@ -4,7 +4,8 @@ ecobee=DP1
 
 # triple-monitor-setup
 if xrandr | grep -w "$ecobee connected"; then
-    xrandr --output eDP1 --mode 1600x900 --pos 0x904 --rotate normal --output DP1 --primary --mode 2560x1440 --pos 1600x364 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 4160x0 --rotate right --output VIRTUAL1 --off
+    # randr --output eDP1 --mode 1600x900 --pos 0x904 --rotate normal --output DP1 --primary --mode 2560x1440 --pos 1600x364 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 4160x0 --rotate right --output VIRTUAL1 --off
+    xrandr --output eDP1 --off --output DP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
     i3-msg "workspace 1, move workspace to output DP1"
     i3-msg "workspace 2, move workspace to output DP1"
     i3-msg "workspace 3, move workspace to output DP1"
@@ -17,6 +18,7 @@ if xrandr | grep -w "$ecobee connected"; then
     notify-send -t 1000 "Triple Monitor Setup Applied"
     exec nitrogen --restore; sleep 1; compton -b;
 elif xrandr | grep -w "$ecobee disconnected"  && xrandr | grep -w "HDMI2 connected"; then
+    # Curved monitor at Home + Laptop
     xrandr --output eDP1 --mode 1600x900 --pos 0x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output VIRTUAL1 --off
     i3-msg "workspace 1, move workspace to output HDMI2"
     i3-msg "workspace 2, move workspace to output HDMI2"
