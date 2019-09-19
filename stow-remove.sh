afab9206e0f0a6014845d84a7c986de09118ef5d
Luis Rivera-Wong <lriverawong@outlook.com>
2019-09-18 22:03:51 -0400

    added ssh-agent user service file and fixed stow scripts

--- stow-remove.sh
+++ stow-remove.sh
@@ -4,22 +4,16 @@
 
 echo "=== Starting to link all dotfiles ==="
 
-stow -Dv conky
 stow -Dv docker
 stow -Dv github-config
 stow -Dv glances
 stow -Dv konsole
 stow -Dv libinput-gestures
 stow -Dv neovim
-stow -Dv profile
-stow -Dv ranger
 stow -Dv scripts
 stow -Dv ssh-agent
-stow -Dv ssh-add
-stow -Dv ssh-askpass
 stow -Dv tmux
 stow -Dv vim
-stow -Dv X
-stow -Dv zshsh
+stow -Dv zsh
 
 echo "=== Done linking all dotfiles ==="
--- stow.sh
+++ stow.sh
@@ -4,22 +4,16 @@
 
 echo "=== Starting to link all dotfiles ==="
 
-stow -v conky
 stow -v docker
 stow -v github-config
 stow -v glances
 stow -v konsole
 stow -v libinput-gestures
 stow -v neovim
-stow -v profile
-stow -v ranger
 stow -v scripts
 stow -v ssh-agent
-stow -v ssh-add
-stow -v ssh-askpass
 stow -v tmux
 stow -v vim
-stow -v X
 stow -v zsh
 
 echo "=== Done linking all dotfiles ==="
