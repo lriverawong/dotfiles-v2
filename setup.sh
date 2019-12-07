3824eaf427b57a2fca761ece67d2ba8bd7776b9b
Luis Rivera-Wong <luis.r@ecobee.com>
2019-12-06 19:47:56 -0500

    initial untested fedora install automation

--- setup.sh
+++ setup.sh
@@ -25,23 +25,27 @@ if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     # zsh history substring search
     git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-    # spaceship prompt
-    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+    
     # --- custom themes ---
     # -- pure zsh theme --
-    git clone https://github.com/sindresorhus/pure.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/pure
-    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+    # git clone https://github.com/sindresorhus/pure.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/pure
+    
 
     # check for existence of zfunctions directory
-    if [ ! -d ~/.zfunctions ]; then
-        mkdir ~/.zfunctions
-    fi
+    # if [ ! -d ~/.zfunctions ]; then
+    #     mkdir ~/.zfunctions
+    # fi
 
     # link
-    ln -s "$HOME/.oh-my-zsh/custom/themes/pure/pure.zsh" "$HOME/.zfunctions/prompt_pure_setup"
-    ln -s "$HOME/.oh-my-zsh/custom/themes/pure/async.zsh" "$HOME/.zfunctions/async"
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+    # ln -s "$HOME/.oh-my-zsh/custom/themes/pure/pure.zsh" "$HOME/.zfunctions/prompt_pure_setup"
+    # ln -s "$HOME/.oh-my-zsh/custom/themes/pure/async.zsh" "$HOME/.zfunctions/async"
+    
 fi
+
+# setup starship
+# spaceship prompt
+zsh -c "git clone https://github.com/denysdovhan/spaceship-prompt.git \"$ZSH_CUSTOM/themes/spaceship-prompt\" && \
+    ln -s \"$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme\" \"$ZSH_CUSTOM/themes/spaceship.zsh-theme\" "
 
 # install kubectx
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
@@ -60,14 +64,63 @@ popd
 # run stow setup
 sh ./stow.sh
 
-# install packages
-sudo pacman -S --noconfirm - < ./packages/arch/pacman-pkglist.txt
+# -- Arch Installs --
+if [[ type pacman &> /dev/null ]]; then
+    # install packages
+    sudo pacman -S --noconfirm - < ./packages/arch/pacman-pkglist.txt
+    # install AUR packages
+    yay -S --noconfirm - < ./packages/arch/aur-pkglist.txt
+elif [[ type dnf &> /dev/null ]]; then
+    # --- Fedora Install ---
+    # fix hostname
+    sudo hostnamectl set-hostname l-t480-f
+    dnf install -y $(grep "^[^#]" ./packages/fedora/pkglist.txt)
+    # enable tlp
+    sudo systemctl enable tlp && sudo systemctl start tlp
+    # multimedia
+    sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+    sudo dnf groupupdate sound-and-video
+    # google chrome
+    sudo dnf config-manager --set-enabled google-chrome
+    sudo dnf install fedora-workstation-repositories
+    sudo dnf config-manager --set-enabled google-chrome
+    sudo dnf check-update
+    sudo dnf install google-chrome-stable
+    # vscode
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    sudo dnf check-update
+    sudo dnf install code
+    # slack
+    sudo dnf copr -y enable jdoss/slack-repo
+    sudo dnf install -y slack-repo
+    sudo dnf install -y slack
+    # flatpak setup
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    # spotify
+    flatpak install -y flathub com.spotify.Client
+    # teams-for-linux
+    flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
+    # gcloud
+    sudo sh -c 'echo -e "[google-cloud-sdk]\nname=Google Cloud SDK\nbaseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg\n\thttps://packages.cloud.google.com/yum/doc/rpm-package-key.gpg" > /etc/yum.repos.d/google-cloud-sdk2.repo'
+    # to-do
+    # - fira-fonts
+    # - keybase
+    # - simplenote
+    # - nordvpn
+    # - openvpn-update-systemd-resolved
+    # - keybase
+    # - thefuck
+    # - gesture [cli, and gui]
+
+else
+    echo "No valid distro found, exiting..."
+    exit 1
+fi
 
 # add user to input group for touchpad gestures
 sudo gpasswd -a $USER input
-
-# install AUR packages
-yay -S --noconfirm - < ./packages/arch/aur-pkglist.txt
 
 # setup helm
 helm init --client-only
